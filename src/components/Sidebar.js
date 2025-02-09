787f996e52c0b3c249725de151119f618a98c140
Seenu Sharma <asprofessionalwork@gmail.com>
2025-02-09 10:56:10 +0530

    Update Sidebar.js

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,6 +2,12 @@ import React from 'react';
 import { CiHome } from "react-icons/ci";
 import { SiYoutubeshorts } from "react-icons/si";
 import { MdOutlineSubscriptions } from "react-icons/md";
+import { FaHistory } from "react-icons/fa";
+import { MdOutlinePlaylistPlay } from "react-icons/md";
+import { MdOutlineWatchLater } from "react-icons/md";
+import { BiLike } from "react-icons/bi";
+import { IoMdTrendingUp } from "react-icons/io";
+import { RiShoppingBag4Line } from "react-icons/ri";
 import {useSelector} from "react-redux";
 
 const sidebarItem = [
@@ -18,89 +24,30 @@ const sidebarItem = [
         title: "Subscription"
     },
     {
-        icons: <CiHome size="24px" />,
-        title: "Home"
+        icons: <FaHistory size="24px" />,
+        title: "History"
     },
     {
-        icons: <SiYoutubeshorts size="24px" />,
-        title: "Short"
+        icons: <MdOutlinePlaylistPlay size="24px" />,
+        title: "Playlist"
     },
     {
-        icons: <MdOutlineSubscriptions size="24px" />,
+        icons: <MdOutlineWatchLater size="24px" />,
+        title: "Watch later "
+    },
+    {
+        icons: <BiLike size="24px" />,
+        title: "Liked"
+    },
+    {
+        icons: <IoMdTrendingUp size="24px" />,
+        title: "Trending"
+    },
+    {
+        icons: <MdOutlineSubscriptions size="24px"  />,
         title: "Subscription"
     },
-    {
-        icons: <CiHome size="24px" />,
-        title: "Home"
-    },
-    {
-        icons: <SiYoutubeshorts size="24px" />,
-        title: "Short"
-    },
-    {
-        icons: <MdOutlineSubscriptions size="24px" />,
-        title: "Subscription"
-    },
-    {
-        icons: <CiHome size="24px" />,
-        title: "Home"
-    },
-    {
-        icons: <SiYoutubeshorts size="24px" />,
-        title: "Short"
-    },
-    {
-        icons: <MdOutlineSubscriptions size="24px" />,
-        title: "Subscription"
-    },
-    {
-        icons: <CiHome size="24px" />,
-        title: "Home"
-    },
-    {
-        icons: <SiYoutubeshorts size="24px" />,
-        title: "Short"
-    },
-    {
-        icons: <MdOutlineSubscriptions size="24px" />,
-        title: "Subscription"
-    },
-    {
-        icons: <CiHome size="24px" />,
-        title: "Home"
-    },
-    {
-        icons: <SiYoutubeshorts size="24px" />,
-        title: "Short"
-    },
-    {
-        icons: <MdOutlineSubscriptions size="24px" />,
-        title: "Subscription"
-    },
-    {
-        icons: <CiHome size="24px" />,
-        title: "Home"
-    },
-    {
-        icons: <SiYoutubeshorts size="24px" />,
-        title: "Short"
-    },
-    {
-        icons: <MdOutlineSubscriptions size="24px" />,
-        title: "Subscription"
-    },
-    {
-        icons: <CiHome size="24px" />,
-        title: "Home"
-    },
-    {
-        icons: <SiYoutubeshorts size="24px" />,
-        title: "Short"
-    },
-    {
-        icons: <MdOutlineSubscriptions size="24px" />,
-        title: "Subscription"
-    },
+    
 ]
 
 const Sidebar = () => { 
